aeb8e3b7a25204b637f5f04c6a271eaf3db74400
Celio Caus Junior <celiocausjunior@MacBook-Air-de-Celio.local>
2021-02-21 16:55:28 -0300

    Set up STS and Docker

--- src/main/java/com/example/cassandra/springbootcassandrademo/ResouceNotFoundException.java
+++ src/main/java/com/example/cassandra/springbootcassandrademo/ResouceNotFoundException.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResouceNotFoundException extends RuntimeException {
 
-    public ResouceNotFoundException(String message) {
+	private static final long serialVersionUID = 1L;
+
+	public ResouceNotFoundException(String message) {
         super(message);
     }
 }
--- src/main/java/com/example/cassandra/springbootcassandrademo/model/Product.java
+++ src/main/java/com/example/cassandra/springbootcassandrademo/model/Product.java
@@ -15,5 +15,19 @@ public class Product {
     @PrimaryKey
     private int id;
     private String name;
+    
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
